a3c439f1582839d5c8133ea1bcb7b7765599afd5
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-05-21 06:12:22 -0400

    add adsr convenience method to instrument

--- notes/snippets.js
+++ notes/snippets.js
@@ -7,7 +7,7 @@ d.synth("saw")
   .scale("min")
   .notes([0, 2, 4, 6], [8, 6, 4, 2])
   .fast(2)
-  .gain(d.env().adsr(0, 1, 0.333, 1))
+  .adsr(0, 1, 0.333, 1)
   .fx(
     d.lpf(d.env(200, 1600).adsr(0.25, 0.5, 0.25, 0.5)),
     d.lpf(d.lfo(200, 2400).wave("saw").speed(0.5).norm()),
@@ -19,7 +19,7 @@ d.synth("sq")
   .scale("min")
   .notes([0, 2, 4, 6], [8, 6, 4, 2])
   .fast(2)
-  .gain(d.env().adsr(0, 1, 0.333, 1))
+  .adsr(0, 1, 0.333, 1)
   .fx(
     d.lpf(d.env(200, 1600).adsr(0.25, 0.5, 0.25, 0.5)),
     d.lpf(d.lfo(200, 1600).wave("sq").off(0.5).speed(2).norm()),
