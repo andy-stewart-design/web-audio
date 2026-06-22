5072d0937133eff46b21c00dc5979c2cbbd08902
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-06-22 06:23:20 -0400

    default style updates

--- packages/editor/src/theme.ts
+++ packages/editor/src/theme.ts
@@ -16,10 +16,13 @@ export const theme = EditorView.theme(
       overscrollBehavior: "contain",
       scrollbarWidth: "thin",
       scrollbarColor: "var(--cm-scrollbar-color) transparent",
+      lineHeight: 1.6,
     },
     ".cm-content": {
       fontFamily: "var(--cm-editor-font-family)",
       caretColor: "transparent",
+      paddingBlockStart: "var(--cm-editor-padding-block-start)",
+      paddingBlockEnd: "var(--cm-editor-padding-block-end)",
     },
     "&.cm-focused .cm-cursor": {
       borderLeft: "var(--cm-cursor-width) solid var(--cm-cursor-color)",
@@ -34,8 +37,8 @@ export const theme = EditorView.theme(
       fontFamily: "var(--cm-editor-font-family)",
       fontSize: "var(--cm-gutter-font-size)",
       userSelect: "none",
-      borderInlineEnd:
-        "var(--cm-gutter-border-width) solid var(--cm-gutter-border-color)",
+      boxShadow:
+        "calc(-1 * var(--cm-gutter-border-width)) 0px 0px 0px var(--cm-gutter-border-color) inset",
     },
     "& .cm-lineNumbers .cm-gutterElement": {
       paddingInline: "0.75rem 0.625rem",
@@ -46,18 +49,26 @@ export const theme = EditorView.theme(
       inlineSize: "var(--cm-line-inline-size)",
     },
     ".cm-activeLine": {
-      backgroundColor: "var(--cm-active-line-blurred-color-background)",
+      backgroundColor: "var(--cm-active-line-color-background-blurred)",
     },
     "&.cm-focused .cm-activeLine": {
       backgroundColor: "var(--cm-active-line-color-background)",
+    },
+    ".cm-gutterElement": {
+      borderInlineEnd: "1px solid transparent",
+      backgroundColor: "transparent",
     },
     ".cm-activeLineGutter": {
       backgroundColor: "transparent",
     },
     "&.cm-focused .cm-activeLineGutter": {
+      borderInlineEnd: "1px solid var(--cm-active-gutter-border-color)",
       backgroundColor: "var(--cm-active-gutter-color-background)",
     },
-    "& .cm-matchingBracket, &.cm-focused .cm-matchingBracket": {
+    "& .cm-matchingBracket": {
+      backgroundColor: "var(--cm-matching-bracket-color-background-blurred)",
+    },
+    "&.cm-focused .cm-matchingBracket": {
       backgroundColor: "var(--cm-matching-bracket-color-background)",
     },
   },
