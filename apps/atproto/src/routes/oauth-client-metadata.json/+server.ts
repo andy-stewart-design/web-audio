c7420a4cfebe90f2c4d6d1d9ebef0ce1f3eb1f3c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-07-10 06:46:32 -0400

    remove dead apps

--- apps/atproto/src/routes/oauth-client-metadata.json/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { getOAuthClient } from '$lib/server/auth/client';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const client = await getOAuthClient();
-	return json(client.clientMetadata);
-};
